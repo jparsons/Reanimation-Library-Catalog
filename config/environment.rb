5d33710f6add41d85a534c5cb754ac7219c57640
unknown <John Parsons@.(none)>
2010-02-08 21:59:37 -0500

    adding easy_roles

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   
   config.gem "authlogic"
+  config.gem "easy_roles", :source => "http://gemcutter.org"
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
