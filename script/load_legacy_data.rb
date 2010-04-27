bb827c3e05df367cd180455684f56a3381d90f99
John Parsons <jparsons@klondikefive.com>
2010-04-27 08:57:44 -0400

    update to legacy script

--- script/load_legacy_data.rb
+++ script/load_legacy_data.rb
@@ -1,33 +1,37 @@
 require 'hpricot'
+
+Item.destroy_all
+
 xml_document = Hpricot(File.open("#{RAILS_ROOT}/db/legacy/item.xml"))
 xml_document.search("//row").each {|node|
-  puts "hello"
   params = {}
-    nodes = %w(title subtitle publisher publisher_city publisher_state publisher_country extent copyright item_size call_number collection_number item_id)
-    fields = %w(title subtitle publisher_name publisher_city publisher_state publisher_country extent copyright item_size call_number collection_name legacy_id)
-    nodes.each_with_index do |name, i|
-      node_set = node.search(name)
-      unless node_set.nil?
-        child = node_set.first
-        unless child.nil? || child.children.nil?
-          unless child.children.first.nil?
-            content = child.children.first.inner_html()
-          end
-        end
-      end
-      content ||= ""
-      params[fields[i].to_sym] = content
+  nodes = %w(title subtitle publisher publisher_city publisher_state publisher_country extent copyright item_size call_number collection_number item_id)
+  fields = %w(title subtitle publisher_name publisher_city publisher_state publisher_country extent copyright item_size call_number collection_name legacy_id)
+  nodes.each_with_index do |name, i|
+    elements = node.search(name+"/data")
+    content = elements.inner_html()
+    if content.blank?
+      elements = node.search(name)
+      content = elements.inner_html()
+    end
+    content ||= ""
+    params[fields[i].to_sym] = content
+    #puts "#{fields[i]} = #{content}"
       
-    end
-    i = Item.create(params)
-    subject_node_list = node.search("subject_name")
-    unless subject_node_list.nil?
-      subjects = subject_node_list.search("DATA")
-      subjects.each {|subject_node|
-        s = Subject.find_or_create_by_name(subject_node.inner_html())
-        i.subjects << s
-      }
-    end
+  end
+  i = Item.create(params)
+  subjects = node.search("subject_name/data")
+  subjects.each {|subject|
+    s = Subject.find_or_create_by_name(subject.inner_html())
+    i.subjects << s
+  }
+    #unless subjects.nil?
+    #  #subjects = subject_node_list.search("data")
+    #  subjects.each {|subject_node|
+    #    s = Subject.find_or_create_by_name(subject_node.search("data").inner_html())
+    #    i.subjects << s
+    #  }
+    #end
     
     
   
