24455e56cecb1e0cd3005adeb40551fa0e63272c
John Parsons <jparsons@klondikefive.com>
2010-06-11 21:32:58 -0400

    reconciling changes

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -3,15 +3,15 @@ class Donor < ActiveRecord::Base
   has_and_belongs_to_many :items
 end
 
+
 # == Schema Information
 #
 # Table name: donors
 #
 #  id                :integer         not null, primary key
-#  first_name        :string(255)
-#  last_name         :string(255)
 #  organization_name :string(255)
 #  created_at        :datetime
 #  updated_at        :datetime
+#  name              :string(255)
 #
 
--- test/unit/work_test.rb
+++ test/unit/work_test.rb
@@ -5,3 +5,18 @@ class WorkTest < ActiveSupport::TestCase
     assert Work.new.valid?
   end
 end
+
+# == Schema Information
+#
+# Table name: works
+#
+#  id           :integer         not null, primary key
+#  title        :string(255)
+#  medium       :string(255)
+#  size         :string(255)
+#  year_created :integer
+#  genre        :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
