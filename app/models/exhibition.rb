e2ddcbdabde4e27109713dbff82525495c1eb018
J Parsons <jparsons@klondikefive.com>
2010-07-19 15:01:52 -0400

    exhibition and works forms

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,6 +1,10 @@
 class Exhibition < ActiveRecord::Base
   belongs_to :exhibition_venue
   has_and_belongs_to_many :works
+  
+  accepts_nested_attributes_for :exhibition_venue
+  accepts_nested_attributes_for :works
+  
 end
 
 # == Schema Information
