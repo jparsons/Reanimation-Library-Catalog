a16f20932c76383529976d9d75dd9db819801f75
John Parsons <jparsons@John-Parsonss-MacBook-Pro.local>
2011-02-13 14:56:45 -0500

    Changed creator display name

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,7 +1,7 @@
 class Creator < ActiveRecord::Base
   belongs_to :item
   def display_name
-    last_name + ", " + first_name + (middle_name.blank? ? "" : ", " + middle_name) + " (" + creator_type + ")"
+    first_name + (middle_name.blank? ? "" : " " + middle_name) + " " + last_name + " (" + creator_type + ")"
   end
   
 end
