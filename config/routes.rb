b6a31bea8930970f5e399e1f437bf86d0d6ca595
john parsons <jparsons@klondikefive.com>
2011-05-09 10:22:37 -0400

    setting subdirectory for deploy site

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,41 @@
 ReanimationLibraryCatalog::Application.routes.draw do
-  match 'search' => 'search#new', :as => :search
-  match 'advanced_search' => 'search#new', :as => :advanced_search, :advanced => 'true'
-  match 'search_results' => 'search#show', :as => :search_results
-  resources :exhibitions
-  resources :vendors
-  resources :digital_assets do
-    collection do
-  get :most_recent
-  end
+  scope "/catalog" do
+    match 'search' => 'search#new', :as => :search
+    match 'advanced_search' => 'search#new', :as => :advanced_search, :advanced => 'true'
+    match 'search_results' => 'search#show', :as => :search_results
+    resources :exhibitions
+    resources :vendors
+    resources :digital_assets do
+      collection do
+    get :most_recent
+    end
   
   
-  end
+    end
 
-  resources :works
-  resources :subjects
-  resources :items do
-    collection do
-  get :acquired
-  get :need_images
-  get :by_call_number
-  get :recent
-  end
+    resources :works
+    resources :subjects
+    resources :items do
+      collection do
+    get :acquired
+    get :need_images
+    get :by_call_number
+    get :recent
+    end
   
   
-  end
+    end
 
-  match 'signup' => 'users#new', :as => :signup
-  match 'logout' => 'user_sessions#destroy', :as => :logout
-  match 'login' => 'user_sessions#new', :as => :login
-  resources :user_sessions
-  resources :digital_asset_ingests
-  resources :users
-  resources :log_entries
-  match 'dashboard' => 'admin_dashboard#index', :as => :admin_dashboard
-  match '/' => 'items#index'
-  match 'root' => 'items#index'
-  match '/:controller(/:action(/:id))'
+    match 'signup' => 'users#new', :as => :signup
+    match 'logout' => 'user_sessions#destroy', :as => :logout
+    match 'login' => 'user_sessions#new', :as => :login
+    resources :user_sessions
+    resources :digital_asset_ingests
+    resources :users
+    resources :log_entries
+    match 'dashboard' => 'admin_dashboard#index', :as => :admin_dashboard
+    match '/' => 'items#index'
+    match 'root' => 'items#index'
+    match '/:controller(/:action(/:id))'
+  end
 end
