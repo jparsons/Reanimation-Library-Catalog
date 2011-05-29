7efececce4939a742a8eff15f56ffeb59046313e
john parsons <jparsons@klondikefive.com>
2011-05-29 10:04:43 -0400

    adding migration for digital_assets_works join table

---
+++ db/migrate/20110529133353_create_digital_assets_works.rb
@@ -0,0 +1,12 @@
+class CreateDigitalAssetsWorks < ActiveRecord::Migration
+  def self.up
+    create_table :digital_assets_works, :id=>false do |t|
+     t.integer :digital_asset_id
+     t.integer :work_id
+    end
+  end
+
+  def self.down
+    drop_table :digital_assets_works
+  end
+end
