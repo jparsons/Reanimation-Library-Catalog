8f4033ae740b53e3bbba819fd8b85b77d94c966e
John Parsons <john@thrivetrm.com>
2020-04-26 16:52:43 -0400

    Adds region to AWS config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,11 @@
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY"],
       aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:                ENV['AWS_REGION'], 
     }
     config.fog_directory  = ENV["AWS_BUCKET"]
-  end
+
 end
